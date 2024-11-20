4c8e5f8f4304084fe9ed4886023b60d5e4cbf8e5
Daniel Rodrigues <capau.2020120iipi0064@aluno.ifpi.edu.br>
2024-11-20 16:38:11 -0300

    aula da imersao 3

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-
 import express from "express";
+import routes from "./src/routes/serverRoutes.js";
 
 const app = express();
+
+routes(app);
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
-});
-
-app.get("/", (req, res) => {
-    res.status(200).send("Boa vindas a imersão");
 });
